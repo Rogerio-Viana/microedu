d969cb1e97b54b567b4da53b855c87a03cdbf846
Rogerio Viana <vrogerio07@gmail.com>
2019-11-02 19:31:08 -0200

    first commit

---
+++ microedu/src/main/java/br/com/microedu/MicroeduApplication.java
@@ -0,0 +1,13 @@
+package br.com.microedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroeduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroeduApplication.class, args);
+	}
+
+}
---
+++ microedu/src/test/java/br/com/microedu/MicroeduApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.microedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroeduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
