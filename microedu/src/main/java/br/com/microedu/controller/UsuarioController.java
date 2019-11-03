ee05c01327f2795cb11b54ce2276c3633f79a4d2
Rogerio Viana <vrogerio07@gmail.com>
2019-11-03 20:30:14 -0200

    menu lateral, fontawaser

--- microedu/src/main/java/br/com/microedu/controller/UsuarioController.java
+++ microedu/src/main/java/br/com/microedu/controller/UsuarioController.java
@@ -2,18 +2,18 @@ package br.com.microedu.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.microedu.freemarker.IndexOfMethod;
 import br.com.microedu.usuario.UsuarioModel;
 import br.com.microedu.usuario.UsuarioService;
 
-@RestController
+@Controller
 @RequestMapping("usuario")
 public class UsuarioController {
 
@@ -22,8 +22,7 @@ public class UsuarioController {
 
 	@RequestMapping(value = { "" }, method = RequestMethod.GET)
 	public ModelAndView buscar() {
-		return new ModelAndView("pages/listagem-usuario")
-				.addObject("usuarios", usuarioService.buscarTodos())
+		return new ModelAndView("pages/listagem-usuario").addObject("usuarios", usuarioService.buscarTodos())
 				.addObject("indexOf", new IndexOfMethod());
 	}
 
--- microedu/src/main/java/br/com/microedu/freemarker/IndexOfMethod.java
+++ microedu/src/main/java/br/com/microedu/freemarker/IndexOfMethod.java
@@ -2,6 +2,7 @@ package br.com.microedu.freemarker;
 
 import java.util.List;
 
+import freemarker.ext.beans.StringModel;
 import freemarker.template.SimpleNumber;
 import freemarker.template.TemplateMethodModel;
 import freemarker.template.TemplateModel;
@@ -10,13 +11,24 @@ import freemarker.template.TemplateModelException;
 @SuppressWarnings("deprecation")
 public class IndexOfMethod implements TemplateMethodModel {
 
+//	@SuppressWarnings("rawtypes")
+//	public TemplateModel exec(List args) throws TemplateModelException {
+//		
+//		if (args.size() != 2) {
+//			throw new TemplateModelException("Wrong arguments");
+//		}
+//		
+//		return new SimpleNumber(((String) args.get(1)).indexOf((String) args.get(0)));
+//	}
+	
 	@SuppressWarnings("rawtypes")
-	public TemplateModel exec(List args) throws TemplateModelException {
+	public String exec(List args) throws TemplateModelException {
 		
 		if (args.size() != 2) {
 			throw new TemplateModelException("Wrong arguments");
 		}
 		
-		return new SimpleNumber(((String) args.get(1)).indexOf((String) args.get(0)));
+		return "Teste!";
 	}
+	
 }
